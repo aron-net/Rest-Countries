a331148c2e501e34570989983619a1220f1cdd78
aron-helu <aronmilahela@gmail.com>
2022-09-09 15:20:32 +0300

    fix stayle

--- src/Components/Continents/Continents.js
+++ src/Components/Continents/Continents.js
@@ -62,7 +62,7 @@ const Continent = () => {
           <button key={id} className="individual-character" type="button" onClick={() => listHandler(name)} onKeyDown={() => listHandler(name)}>
             <Link to={`/${name}`}>
               {/* <img clasName="afro" src={afro} alt="aron" /> */}
-              <h2 className="continent-title">{name}</h2>
+              <h2 className="h2">{name}</h2>
             </Link>
           </button>
         ))}
